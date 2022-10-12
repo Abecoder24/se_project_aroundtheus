c8c6a11a80012dbde9d52f40d91e0c8caadb0693
Abraham snetter <abrahamsnetter@gmail.com>
2022-10-12 16:12:09 -0700

    mke

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let initialCards = [{name: "Yosemite Valley",
+link:"../images/yosemite-valley.jpg"},
+{name:"Lake Louise", link: "../images/lake-louise.png"},
+{name:"Bald Mountains", link: "../images/bald-mountains.png"}, 
+{name:"Latemar", link:"../images/latemar.png"}, 
+{name: "Vanoise National Park", link: "../images/vanoise-national-park.jpg"}, 
+{name: "Lago di Braies", link: "../images/lago-di-braise.png"}];
