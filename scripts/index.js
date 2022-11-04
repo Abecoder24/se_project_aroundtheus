75e51f12e772aaa02fcd21c71c2488a3aa8a83ea
Abraham snetter <abrahamsnetter@gmail.com>
2022-11-04 16:05:31 -0700

    buttons

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,14 @@ link:"../images/yosemite-valley.jpg"},
 {name:"Latemar", link:"../images/latemar.png"}, 
 {name: "Vanoise National Park", link: "../images/vanoise-national-park.jpg"}, 
 {name: "Lago di Braies", link: "../images/lago-di-braise.png"}];
+ 
+const profileCloseButton = document.querySelector(".modal__close")
+ const profileEditButton = document.querySelector(".profile__button");
+ const modal = document.querySelector(".modal");
+
+ profileEditButton.addEventListener("click", () => {
+ modal.classList.add("modal__opened")
+ })
+profileCloseButton.addEventListener("click", () =>{
+    modal.classList.remove("modal__opened")
+})
